ec790eddb634bb74333133ca5bfb432e6c2f4d4b
muriloux <murilomelo.code@gmail.com>
2022-12-21 13:32:14 -0300

    env

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 import bot from "./bot";
+import * as dotenv from "dotenv";
+import fs from "fs";
+import path from "path";
+dotenv.config();
+
+fs.writeFileSync(
+  path.resolve(__dirname, "..", "cache", "auth_info_multi.json"),
+
+  process.env.AUTH_STATE as string
+);
 
 bot();
